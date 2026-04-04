7fe2156e2cf59ea1575c285234a528e1dc6e3f06
BigDumbYak-Git <bigdumbyak@gmail.com>
2026-04-04 12:01:45 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,10 +2,7 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (
-      url.hostname === "www.abps.ca" ||
-      url.hostname === "abpswebsite.bigdumbyak.workers.dev"
-    ) {
+    if (url.hostname !== "abps.ca") {
       return Response.redirect(
         "https://abps.ca" + url.pathname + url.search,
         301
